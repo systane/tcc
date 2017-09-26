51e58e8b1bba958f99f838bc5fab06604f5cd37f
Luis Fernando <lf.silva@outlook.com>
2017-09-25 21:59:00 -0300

    Add files via upload

--- comparaMatriz.cpp
+++ comparaMatriz.cpp
@@ -24,7 +24,7 @@ void readFile (std::vector<double> & vector, string file, string className){
 	string line;
 	string trash = "valor:";
 	double value;
-	file = file + "class:" +className;
+	file = file + "/class:" +className;
 
 
 	ifstream referenceFile (file.c_str());
@@ -51,15 +51,26 @@ void initArray(int (&array)[55]){
 	for(int i = 0; i < 55; i++){
 		array[i] = 0;
 	}
-	
 }
 
+
+void printFile (string sequence, int score){
+	string outputFile = "classificacoes/distancia_euclidiana/Resultados";
+
+	ofstream outFile(outputFile.c_str(), std::ofstream::app); //modo apendice, não substitui o conteúdo já existe no txt
+	if(outFile.is_open()){
+		 outFile<< sequence << " - " << score << endl;
+		 outFile.close();
+	}
+	else
+		cout << "Arquivo Resultados nao pode ser aberto"<<endl;
+}
 
 
 int main (int argc, char* arg[]){
 
 	string sequence = arg[1];
-	string referece_file_path = "classificacoes/distancia_euclidiana/12106573948/";//+sequence;
+	string referece_file_path = "classificacoes/distancia_euclidiana/"+sequence;
 
 	cout<<"Numero de sequencia: "<<sequence<<endl;
 
@@ -141,102 +152,103 @@ int main (int argc, char* arg[]){
 	
 	// pega os melhores resultados
 	for(int i = 0; i < 55; ++i){
-		double max = classCalen[i];
+		double maxValue;
+		double max[11];
+		max[0]  = classAbrirCalc[i];
+		max[1]  = classCalen[i];
+		max[2]  = classAlarme[i];
+		max[3]  = classBoaNoite[i];
+		max[4]  = classBomDia[i];
+		max[5]  = classComoTempo[i];
+		max[6]  = classOi[i];
+		max[7]  = classPesq[i];
+		max[8]  = classSair[i];
+		max[9]  = classVaiCho[i];
+		max[10] = classVerNot[i];
 
 
-		if(max < classCalen[i]){
-			max = classCalen[i];
+		maxValue = *std::max_element(max, max+11);
+		bestResults.push_back(maxValue);
+
+		if(maxValue == classCalen[i]){
 			positionCalen[i] = positionCalen[i] + 1;
 			++qntCalen;
 
 		}
 
-		if(max < classAbrirCalc[i]){
-			max = classAbrirCalc[i];
+		if(maxValue == classAbrirCalc[i]){
 			positionAbrirCalc[i] = positionAbrirCalc[i] + 1;
 			++qntAbrirCalc;
 
 		}
 
-		if(max < classAlarme[i]){
-			max = classAlarme[i];
+		if(maxValue == classAlarme[i]){
 			positionAlarme[i] = positionAlarme[i] + 1;
 			++qntAlarme;
 		}
 
-		if(max < classBoaNoite[i]){
-			max = classBoaNoite[i];
+		if(maxValue == classBoaNoite[i]){
 			positionBoaNoite[i] = positionBoaNoite[i] + 1;
 			++qntBoaNoite;
 		}
 
-		if(max < classBomDia[i]){
-			max = classBomDia[i];
+		if(maxValue == classBomDia[i]){
 			positionBomDia[i] = positionBomDia[i] + 1;
 			++qntBomDia;
 		}
 
-		if(max < classComoTempo[i]){
-			max = classComoTempo[i];
+		if(maxValue == classComoTempo[i]){
 			positionComoTempo[i] = positionComoTempo[i] + 1;
 			++qntComoTempo;
 		}
 
-		if(max < classOi[i]){
-			max = classOi[i];
+		if(maxValue == classOi[i]){
 			positionOi[i] = positionOi[i] + 1;
 			++qntOi;
 		}
 
-		if(max < classPesq[i]){
-			max = classPesq[i];
+		if(maxValue == classPesq[i]){
 			positionPesq[i] = positionPesq[i] + 1;
 			++qntPesq;
 		}
 
-		if(max < classSair[i]){
-			max = classSair[i];
+		if(maxValue == classSair[i]){
 			positionSair[i] = positionSair[i] + 1;
 			++qntSair;
 		}
 
-		if(max < classVaiCho[i]){
-			max = classVaiCho[i];
+		if(maxValue == classVaiCho[i]){
 			positionVaiCho[i] = positionVaiCho[i] + 1;
 			++qntVaiCho;
 		}
 
-		if(max < classVerNot[i]){
-			max = classVerNot[i];
+		if(maxValue == classVerNot[i]){
 			positionVerNot[i] = positionVerNot[i] + 1;
 			++qntVerNot;
 		}
-
-		bestResults.push_back(max);
-
 	}
-	cout<<endl;
-	cout<<qntCalen<<endl;
-	cout<<qntAbrirCalc<<endl;
-	cout<<qntAlarme<<endl;
-	cout<<qntBoaNoite<<endl;
-	cout<<qntBomDia<<endl;
-	cout<<qntComoTempo<<endl;
-	cout<<qntOi<<endl;
-	cout<<qntPesq<<endl;
-	cout<<qntSair<<endl;
-	cout<<qntVaiCho<<endl;
-	cout<<qntVerNot<<endl;
-	cout<<endl;
+
 	// preenche a matriz
 	for(int k = 0; k < 55; ++k){
+
+		if(qntCalen > 0){
+			int j = 0;
+			while(qntCalen > 0){
+				if(positionCalen[j] == 1){
+					int linha = (int)j / 5;
+					matrix[linha][0] += 1;
+					--qntCalen;
+				}
+			++j;
+			}
+		}
 
 		if(qntAbrirCalc > 0){
 			int j = 0;
 			while(qntAbrirCalc > 0){
-				if(positionAbrirCalc[j] >= 1){
-					int linha = j % 5;
-					matrix[linha][0] += 1;
+				if(positionAbrirCalc[j] == 1){
+					int linha = (int)j / 5;
+					matrix[linha][1] += 1;
 					--qntAbrirCalc;
 				}
 			++j;
@@ -247,101 +259,10 @@ int main (int argc, char* arg[]){
 		if(qntAlarme > 0){
 			int j = 0;
 			while(qntAlarme > 0){
-				if(positionAlarme[j] >= 1){
-					int linha = j % 5;
-					matrix[linha][1] += 1;
-					--qntAlarme;
-				}
-			++j;
-			}
-		}
-
-
-		if(qntCalen > 0){
-			int j = 0;
-			while(qntCalen > 0){
-				if(positionCalen[j] >= 1){
-					int linha = j % 5;
+				if(positionAlarme[j] == 1){
+					int linha = (int)j / 5;
 					matrix[linha][2] += 1;
-					--qntCalen;
-				}
-			++j;
-			}
-		}
-
-
-		if(qntPesq > 0){
-			int j = 0;
-			while(qntPesq > 0){
-				if(positionPesq[j] >= 1){
-					int linha = j % 5;
-					matrix[linha][3] += 1;
-					--qntPesq;
-				}
-			++j;
-			}
-		}
-
-
-		if(qntSair > 0){
-			int j = 0;
-			while(qntSair > 0){
-				if(positionSair[j] >= 1){
-					int linha = j % 5;
-					matrix[linha][4] += 1;
-					--qntSair;
-				}
-			++j;
-			}
-		}
-
-
-		if(qntOi > 0){
-			int j = 0;
-			while(qntOi > 0){
-				if(positionOi[j] >= 1){
-					int linha = j % 5;
-					matrix[linha][5] += 1;
-					--qntOi;
-				}
-			++j;
-			}
-		}
-
-
-		if(qntVaiCho > 0){
-			int j = 0;
-			while(qntVaiCho > 0){
-				if(positionVaiCho[j] >= 1){
-					int linha = j % 5;
-					matrix[linha][6] += 1;
-					--qntVaiCho;
-				}
-			++j;
-			}
-		}
-
-
-		if(qntVerNot > 0){
-			int j = 0;
-			while(qntVerNot > 0){
-				if(positionVerNot[j] >= 1){
-					int linha = j % 5;
-					matrix[linha][7] += 1;
-					--qntVerNot;
-				}
-			++j;
-			}
-		}
-
-
-		if(qntComoTempo > 0){
-			int j = 0;
-			while(qntComoTempo > 0){
-				if(positionComoTempo[j] >= 1){
-					int linha = j % 5;
-					matrix[linha][8] += 1;
-					--qntComoTempo;
+					--qntAlarme;
 				}
 			++j;
 			}
@@ -351,9 +272,9 @@ int main (int argc, char* arg[]){
 		if(qntBoaNoite > 0){
 			int j = 0;
 			while(qntBoaNoite > 0){
-				if(positionBoaNoite[j] >= 1){
-					int linha = j % 5;
-					matrix[linha][9] += 1;
+				if(positionBoaNoite[j] == 1){
+					int linha = (int)j / 5;
+					matrix[linha][3] += 1;
 					--qntBoaNoite;
 				}
 			++j;
@@ -364,15 +285,92 @@ int main (int argc, char* arg[]){
 		if(qntBomDia > 0 ){
 			int j = 0;
 			while(qntBomDia > 0){
-				if(positionBomDia[j] >= 1){
-					int linha = j % 5;
-					matrix[linha][10] += 1;
+				if(positionBomDia[j] == 1){
+					int linha = (int)j / 5;
+					matrix[linha][4] += 1;
 					--qntBomDia;
 				}
 			++j;
 			}
 		}
 
+		if(qntComoTempo > 0){
+			int j = 0;
+			while(qntComoTempo > 0){
+				if(positionComoTempo[j] == 1){
+					int linha = (int)j / 5;
+					matrix[linha][5] += 1;
+					--qntComoTempo;
+				}
+			++j;
+			}
+		}
+
+		if(qntOi > 0){
+			int j = 0;
+			while(qntOi > 0){
+				if(positionOi[j] == 1){
+					int linha = (int)j / 5;
+					matrix[linha][6] += 1;
+					--qntOi;
+				}
+			++j;
+			}
+
+
+		if(qntPesq > 0){
+			int j = 0;
+			while(qntPesq > 0){
+				if(positionPesq[j] == 1){
+					int linha = (int)j / 5;
+					matrix[linha][7] += 1;
+					--qntPesq;
+				}
+			++j;
+			}
+		}
+
+
+		if(qntSair > 0){
+			int j = 0;
+			while(qntSair > 0){
+				if(positionSair[j] == 1){
+					int linha = (int)j / 5;
+					matrix[linha][8] += 1;
+					--qntSair;
+				}
+			++j;
+			}
+		}
+
+	
+		}
+
+
+		if(qntVaiCho > 0){
+			int j = 0;
+			while(qntVaiCho > 0){
+				if(positionVaiCho[j] == 1){
+					int linha = (int)j / 5;
+					matrix[linha][9] += 1;
+					--qntVaiCho;
+				}
+			++j;
+			}
+		}
+
+
+		if(qntVerNot > 0){
+			int j = 0;
+			while(qntVerNot > 0){
+				if(positionVerNot[j] == 1){
+					int linha = (int)j / 5;
+					matrix[linha][10] += 1;;
+					--qntVerNot;
+				}
+			++j;
+			}
+		}
 	}
 		
 
@@ -382,14 +380,20 @@ int main (int argc, char* arg[]){
 	for(int i = 0; i < 11; ++i){
 		diagonal = diagonal + matrix[i][i];
 	}
+	cout<<"Score: "<<diagonal<<endl;
+
+
+	printFile(sequence, diagonal);
+
+
 
 	//imprimir em um txt
-	for(int i = 0; i < 11; ++i){
-		for(int j = 0; j < 11; ++j){
-			cout<<matrix[i][j]<<"  ";
-		}
-		cout<<endl;
-	}
+	// for(int i = 0; i < 11; ++i){
+	// 	for(int j = 0; j < 11; ++j){
+	// 		cout<<matrix[i][j]<<"  ";
+	// 	}
+	// 	cout<<endl;
+	// }
 	
 
 
